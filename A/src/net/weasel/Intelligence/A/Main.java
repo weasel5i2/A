9a7aa4940d7e4506f01674a9d6ba746e1de2f9c5
weasel5i2 <adrian@weasel.net>
2013-01-13 21:58:30 -0600

    Initial commit.

---
+++ A/src/net/weasel/Intelligence/A/Main.java
@@ -0,0 +1,18 @@
+package net.weasel.Intelligence.A;
+
+public class Main {
+
+	public Main() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
